9c254280a76fb88f4c4f3be607c415ffd67ebcc6
Kiruidyno <kirui472@gmail.com>
2023-03-23 16:51:40 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form=document.querySelector('form');
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+  });
+});
+
+
+const deleteButtons = document.querySelectorAll('.delete-button');
+
+deleteButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const listItem = button.parentNode;
+    listItem.remove();
+  });
 });
